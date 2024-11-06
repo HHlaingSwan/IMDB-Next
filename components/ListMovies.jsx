030442e3c2c555e05317ae6a71667155fb3e5aa5
HHlaingSwan <168323270+HHlaingSwan@users.noreply.github.com>
2024-11-06 19:18:44 +0630

    first commit

--- components/ListMovies.jsx
+++ components/ListMovies.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import MovieCard from "./MovieCard";
 import { cn } from "@/lib/utils";
 
-const ListMovies = ({ Movies, Title, isVertical }) => {
+const ListMovies = ({ Movies, Title }) => {
 	return (
 		<>
 			<div className=' border-b mb-4 flex relative items-center justify-between border-b-neutral-200 mx-4 md:mx-10 py-2'>
@@ -11,7 +11,7 @@ const ListMovies = ({ Movies, Title, isVertical }) => {
 					{Title}
 					<span className='w-32 h-1 bg-red-600 rounded-md inline-block absolute left-0  -bottom-[1.8px]' />
 				</h1>
-				<Link href={"/movies/now-playing"}>
+				<Link href={"/ViewAll"}>
 					<p className='text-xs md:text-sm text-white bg-neutral-600 px-4 py-2 rounded-sm cursor-pointer '>
 						See All
 					</p>
